cb9a66acd59298cce2283efe36fde153b05a4257
Rawda Fathy <eng.rawda.fathy.1@gmail.com>
2025-03-13 06:57:20 +0200

    #added_sub_category

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
-const dbConnnection = () => {
-    mongoose.connect(process.env.DB_URI).then ((conn) => {
-        console.log(`Connected to MongoDB ${conn.connection.host}`);
-        })
-        .catch((err) => {
-            console.error (`Database  Error:${err}`);
-            process.exit(1);   
-        });
+
+const dbConnection = () => {
+  mongoose
+    .connect(process.env.DB_URI)
+    .then((conn) => {
+      console.log(`Database Connected: ${conn.connection.host}`);
+    })
+    // .catch((err) => {
+    //   console.error(`Database Error: ${err}`);
+    //   process.exit(1);
+    // });
 };
-module.exports = dbConnnection;
+
+module.exports = dbConnection;
--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 
 dotenv.config({ path: 'config.env' });
+const ApiError = require('./utils/apiError');
+const globalError = require('./middlewares/errorMiddleware');
 const dbConnection = require('./config/database');
+// Routes
 const categoryRoute = require('./routes/categoryRoute');
+const subCategoryRoute = require('./routes/subCategoryRoute');
+const brandRoute = require('./routes/brandRoute');
 
 // Connect with db
 dbConnection();
@@ -22,17 +27,26 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount Routes
 app.use('/api/v1/categories', categoryRoute);
+app.use('/api/v1/subcategories', subCategoryRoute);
+app.use('/api/v1/brands', brandRoute);
+
+app.all('*', (req, res, next) => {
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
+});
+
+// Global error handling middleware for express
+app.use(globalError);
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App running running on port ${PORT}`);
 });
-// Test Route
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
 
-
-app.post('/', (req, res) => {
-    res.json({ message: 'POST request received!', data: req.body });
+// Handle rejection outside express
+process.on('unhandledRejection', (err) => {
+  console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}`);
+  server.close(() => {
+    console.error(`Shutting down....`);
+    process.exit(1);
+  });
 });
